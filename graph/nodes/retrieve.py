0725b6d96b0a685a748353f405c02dff516cb4b3
binhtran <binh.tran@eastagile.com>
2026-09-18 19:27:13 +0700

    Implement the retrieve
    (cherry picked from commit fd20c8be9b08208e77b9bb871da4771d4d3bba16)

--- graph/nodes/retrieve.py
+++ graph/nodes/retrieve.py
@@ -4,3 +4,7 @@ from graph.state import GraphState
 from ingestion import retriever
 
 def retrieve(state: GraphState) -> Dict[str, Any]:
+    question = state["question"]
+    documents = retriever.invoke(question)
+    
+    return {"documents": documents, "question": question}
