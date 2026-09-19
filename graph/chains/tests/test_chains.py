3d76d33fb73a57a7d6c5ce2024dd9686b054671a
binhtran <binh.tran@eastagile.com>
2026-09-19 15:54:18 +0700

    Implement Grader assessing whether an answer addresses / resolves a question
    Implement Grader assessing whether an answer addresses / resolves a question
    
    (cherry picked from commit 7461106244bfb73ed1ad0c3298d8466d4a8cea2f)

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from graph.chains.retriever_grade import GradeDocuments, retrieval_grader
+from graph.chains.hallucination_grader import GradeHallucinations, hallucination_grader
 from graph.chains.generation import generation_chain
 
 from ingestion import retriever
@@ -40,3 +41,32 @@ def test_generation_chain() -> None:
     })
 
     assert generation is not None
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    
+    docs_txt = [doc.page_content for doc in docs]
+
+    generation = generation_chain.invoke({
+        "context": docs_txt,
+        "question": question,
+    })
+    
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs_txt, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": [doc.page_content for doc in docs],
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
